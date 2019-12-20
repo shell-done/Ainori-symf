fce28f6563d3f0ae162f0a4986416b78218357b2
Alexandre <alexandre.thomas@isen-ouest.yncrea.fr>
2019-12-20 16:58:29 +0100

    Create CRUD for categorie entity

---
+++ src/BackOfficeBundle/Form/CategorieType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BackOfficeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategorieType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('categorie');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BackOfficeBundle\Entity\Categorie'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'backofficebundle_categorie';
+    }
+
+
+}
