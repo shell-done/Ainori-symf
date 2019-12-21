4511f5c4d5ad96c21e079317aba878bedf5c03b8
Alexandre <alexandre.thomas@isen-ouest.yncrea.fr>
2019-12-21 16:58:24 +0100

    Create CRUD for covoiturage entity

---
+++ src/BackOfficeBundle/Controller/CovoiturageController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace BackOfficeBundle\Controller;
+
+use BackOfficeBundle\Entity\Covoiturage;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Covoiturage controller.
+ *
+ */
+class CovoiturageController extends Controller
+{
+    /**
+     * Lists all covoiturage entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $covoiturages = $em->getRepository('BackOfficeBundle:Covoiturage')->findAll();
+
+        return $this->render('@BackOffice/covoiturage/index.html.twig', array(
+            'covoiturages' => $covoiturages,
+        ));
+    }
+
+    /**
+     * Creates a new covoiturage entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $covoiturage = new Covoiturage();
+        $form = $this->createForm('BackOfficeBundle\Form\CovoiturageType', $covoiturage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($covoiturage);
+            $em->flush();
+
+            return $this->redirectToRoute('covoiturage_show', array('id' => $covoiturage->getId()));
+        }
+
+        return $this->render('@BackOffice/covoiturage/new.html.twig', array(
+            'covoiturage' => $covoiturage,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a covoiturage entity.
+     *
+     */
+    public function showAction(Covoiturage $covoiturage)
+    {
+        $deleteForm = $this->createDeleteForm($covoiturage);
+
+        return $this->render('@BackOffice/covoiturage/show.html.twig', array(
+            'covoiturage' => $covoiturage,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing covoiturage entity.
+     *
+     */
+    public function editAction(Request $request, Covoiturage $covoiturage)
+    {
+        $deleteForm = $this->createDeleteForm($covoiturage);
+        $editForm = $this->createForm('BackOfficeBundle\Form\CovoiturageType', $covoiturage);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('covoiturage_edit', array('id' => $covoiturage->getId()));
+        }
+
+        return $this->render('@BackOffice/covoiturage/edit.html.twig', array(
+            'covoiturage' => $covoiturage,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a covoiturage entity.
+     *
+     */
+    public function deleteAction(Request $request, Covoiturage $covoiturage)
+    {
+        $form = $this->createDeleteForm($covoiturage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($covoiturage);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('covoiturage_index');
+    }
+
+    /**
+     * Creates a form to delete a covoiturage entity.
+     *
+     * @param Covoiturage $covoiturage The covoiturage entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Covoiturage $covoiturage)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('covoiturage_delete', array('id' => $covoiturage->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/BackOfficeBundle/Form/CovoiturageType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BackOfficeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CovoiturageType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('created')->add('updated')->add('idCo2')->add('idTrajet')->add('idTypeCovoit')->add('idUtilisateur');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BackOfficeBundle\Entity\Covoiturage'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'backofficebundle_covoiturage';
+    }
+
+
+}
