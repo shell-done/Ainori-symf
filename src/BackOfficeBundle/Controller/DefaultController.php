d80948c39760215b12ca1474e21baaee2987180f
Alexandre <alexandre.thomas@isen-ouest.yncrea.fr>
2019-12-21 01:31:13 +0100

    Fix issue about incorrect default route

--- src/BackOfficeBundle/Controller/DefaultController.php
+++ src/BackOfficeBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BackOfficeBundle:Default:index.html.twig');
+        return $this->render('@BackOffice/Default/index.html.twig');
     }
 }
