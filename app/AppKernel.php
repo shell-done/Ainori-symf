c37aa10024639b6c8515719cd958a5d4b3396afb
Margaux DOUDET <margaux.doudet@isen-ouest.yncrea.fr>
2019-12-23 19:28:40 +0100

    Create WebServiceBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new BackOfficeBundle\BackOfficeBundle(),
+            new WebServiceBundle\WebServiceBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
