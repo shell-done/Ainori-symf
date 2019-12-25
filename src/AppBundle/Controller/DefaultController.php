e304f114f48fbb8150f7884d34c25cf9ed320d94
Alexandre <alexandre.thomas@isen-ouest.yncrea.fr>
2019-12-25 18:53:11 +0100

    Setup the authentification system for back office

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,11 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/", name="homepage")
-     */
+class DefaultController extends Controller {
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
