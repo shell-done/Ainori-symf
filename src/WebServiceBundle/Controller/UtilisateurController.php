1902c26cd77b21308d18ed212f7fec9c73ed5349
Margaux DOUDET <margaux.doudet@isen-ouest.yncrea.fr>
2020-01-04 22:42:31 +0100

    [API] Create /utilisateur/{id}/modify route

--- src/WebServiceBundle/Controller/UtilisateurController.php
+++ src/WebServiceBundle/Controller/UtilisateurController.php
@@ -94,4 +94,56 @@ class UtilisateurController extends Controller {
         return $response;
      }
 
+     public function modifyUtilisateurAction(Request $request, $id) {
+        $erreur = FALSE;
+
+        $utilisateurRepo = $this->getDoctrine()->getRepository(Utilisateur::class);
+        $utilisateur = $utilisateurRepo->findOneById($id);
+
+        $form = $this->createForm('WebServiceBundle\Form\UtilisateurType', $utilisateur);
+        
+        $json = $request->getContent();
+        if ($decodedJson = json_decode($json, true)) {
+            $data = $decodedJson;
+        } else {
+            $data = $request->request->all();
+        }
+        $formData = [];
+        foreach ($form->all() as $name => $field) {
+            if (isset($data[$name])) {
+                $formData[$name] = $data[$name];
+            }
+        }
+    
+        $form->submit($formData);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($utilisateur);
+            $em->flush();
+        }
+        else {
+            $erreur = TRUE;
+        }
+        
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+    
+        $response = new Response();
+        
+        if($erreur) {
+            $response->setContent(json_encode((string) $form->getErrors(true, false)));
+            $response->setStatusCode(400);
+        } else {
+            $response->setContent($serializer->serialize($utilisateur, 'json'));
+            $response->setStatusCode(201);
+        }
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        
+        return $response;
+     }
+
 }
