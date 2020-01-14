4e33275c28c67b778aad2f6d3bde526e9fe4e667
shell-done <alexandre.thomas@isen-ouest.yncrea.fr>
2020-01-14 23:52:46 +0100

    [BO] Add comments on BackOfficeBundle forms

--- src/BackOfficeBundle/Form/UtilisateurType.php
+++ src/BackOfficeBundle/Form/UtilisateurType.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Fichier du formulaire 'UtilisateurType'
+ * 
+ * Ce fichier a été généré par Symfony, pour plus d'informations :
+ * https://symfony.com/doc/current/bundles/SensioGeneratorBundle/commands/generate_doctrine_crud.html
+ * 
+ * @author Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
+ * @version 1.0.0
+ * @package BackOfficeBundle
+ */
+
 namespace BackOfficeBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
@@ -7,10 +18,16 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * Formulaire utilisé pour la création et l'édition d'entité 'Utilisateur' du CRUD
+ */
 class UtilisateurType extends AbstractType
 {
     /**
-     * {@inheritdoc}
+     * Créer le formulaire pour l'entité
+     * 
+     * @param FormBuilderInterface $builder le constructeur de formulaire
+     * @param array $options les options passées au formulaire
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -25,23 +42,33 @@ class UtilisateurType extends AbstractType
                 ->add('adresse')
                 ->add('categorie', null, ["label" => "Catégorie"])
                 ->add('ville');
-    }/**
-     * {@inheritdoc}
+    }
+    
+    /**
+     * Vérifie la validité des différents champs du formulaire
+     * 
+     * @param OptionsResolver $resolver l'objet qui vérifie le formulaire
      */
     public function configureOptions(OptionsResolver $resolver)
     {
+        // La vérification se fait à partir des annotations @Assert dans la classe
+        // relative à l'entité
         $resolver->setDefaults(array(
             'data_class' => 'BackOfficeBundle\Entity\Utilisateur'
         ));
     }
 
     /**
-     * {@inheritdoc}
+     * Retourne le préfixe du nom du formulaire.
+     * 
+     * Ce préfixe est utilisée pour nommé le formulaire et est composé
+     * du nom du bundle suivit de celui de la vue
+     * Exemple : 'backofficebundle_categorie'
+     * 
+     * @return string le prefix associé à la vue
      */
     public function getBlockPrefix()
     {
         return 'backofficebundle_utilisateur';
     }
-
-
 }
--- src/BackOfficeBundle/Form/VoitureType.php
+++ src/BackOfficeBundle/Form/VoitureType.php
@@ -1,38 +1,65 @@
 <?php
 
+/**
+ * Fichier du formulaire 'VoitureType'
+ * 
+ * Ce fichier a été généré par Symfony, pour plus d'informations :
+ * https://symfony.com/doc/current/bundles/SensioGeneratorBundle/commands/generate_doctrine_crud.html
+ * 
+ * @author Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
+ * @version 1.0.0
+ * @package BackOfficeBundle
+ */
+
 namespace BackOfficeBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * Formulaire utilisé pour la création et l'édition d'entité 'Voiture' du CRUD
+ */
 class VoitureType extends AbstractType
 {
     /**
-     * {@inheritdoc}
+     * Créer le formulaire pour l'entité
+     * 
+     * @param FormBuilderInterface $builder le constructeur de formulaire
+     * @param array $options les options passées au formulaire
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('modele', null, ["label" => "Modèle"])
                 ->add('marque')
                 ->add('typeVehicule', null, ["label" => "Type de véhicule"]);
-    }/**
-     * {@inheritdoc}
+    }
+    
+    /**
+     * Vérifie la validité des différents champs du formulaire
+     * 
+     * @param OptionsResolver $resolver l'objet qui vérifie le formulaire
      */
     public function configureOptions(OptionsResolver $resolver)
     {
+        // La vérification se fait à partir des annotations @Assert dans la classe
+        // relative à l'entité
         $resolver->setDefaults(array(
             'data_class' => 'BackOfficeBundle\Entity\Voiture'
         ));
     }
 
     /**
-     * {@inheritdoc}
+     * Retourne le préfixe du nom du formulaire.
+     * 
+     * Ce préfixe est utilisée pour nommé le formulaire et est composé
+     * du nom du bundle suivit de celui de la vue
+     * Exemple : 'backofficebundle_categorie'
+     * 
+     * @return string le prefix associé à la vue
      */
     public function getBlockPrefix()
     {
         return 'backofficebundle_voiture';
     }
-
-
 }
