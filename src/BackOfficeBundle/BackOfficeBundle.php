7644a83421e72c9a9ad9168813b861e0dedc66cf
shell-done <alexandre.thomas@isen-ouest.yncrea.fr>
2020-01-15 00:10:57 +0100

    [BO] Add comments on bundles main classes

--- src/BackOfficeBundle/BackOfficeBundle.php
+++ src/BackOfficeBundle/BackOfficeBundle.php
@@ -1,12 +1,7 @@
 <?php
 
 /**
- * Fichier de base de 'BackOfficeBundle'
- * 
- * Ce bundle est utilisé pour gérer les différentes pages du back office
- * qui sont :
- *  - La page principale avec les statistiques d'utilisation du service
- *  - Les différentes pages du CRUD pour chacune des tables de la base de données
+ * Fichier de base du bundle 'BackOfficeBundle'
  * 
  * @author Alexandre THOMAS <alexandre.thomas@isen-ouest.yncrea.fr>
  * @version 1.0.0
@@ -17,6 +12,14 @@ namespace BackOfficeBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Fichier de base du bundle 'BackOfficeBundle'
+ * 
+ * Ce bundle est utilisé pour gérer les différentes pages du back office
+ * qui sont :
+ *  - La page principale avec les statistiques d'utilisation du service
+ *  - Les différentes pages du CRUD pour chacune des tables de la base de données
+ */
 class BackOfficeBundle extends Bundle
 {
 }
--- src/WebServiceBundle/WebServiceBundle.php
+++ src/WebServiceBundle/WebServiceBundle.php
@@ -1,9 +1,24 @@
 <?php
 
+/**
+ * Fichier de base du bundle 'WebServiceBundle'
+ * 
+ * @author Margaux DOUDET <margaux.doudet@isen-ouest.yncrea.fr>
+ * @version 1.0.0
+ * @package WebServiceBundle
+ */
+
 namespace WebServiceBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Fichier de base du bundle 'WebServiceBundle'
+ * 
+ * Ce bundle est utilisé pour servir les requêtes API consacrées
+ * au front office Angular.
+ * Une documentation détaillée de l'API est disponible avec le projet
+ */
 class WebServiceBundle extends Bundle
 {
 }
